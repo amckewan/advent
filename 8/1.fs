b32dccb95a8b57669573200c1bfc559208ca403f
Andrew McKewan <andrew.mckewan@gmail.com>
2025-12-10 14:05:58 -0500

    WIP

--- 8/1.fs
+++ 8/1.fs
@@ -66,32 +66,94 @@ T{ 1 3 DISTANCE -> 1 3 DIST }T
 
 : ZEROS  0  #BOXES 0 DO  #BOXES 0 DO  J I DISTANCE 0= IF 1+ THEN  LOOP LOOP ;
 
+
 : SHORTEST ( floor -- b1 b2 )   0 0 ROT 1+ -1 ( b1 b2 floor+1 shortest )
-    #BOXES 0 DO  #BOXES 0 DO
+    #BOXES 0 DO  #BOXES 0 DO ( 1,000,000 times )
         2DUP I J DISTANCE -ROT WITHIN ( > floor and < shortest )
         IF  DROP NIP NIP  I J  ROT  I J DISTANCE  THEN
     LOOP LOOP 2DROP ;
 
-\ Connection:   link, b1, b2
-\ Circuit:      link, connection
+: .() ( n )  ."   (" 0 .R ." ) " ;
+: L>H ( b1 b2 -- low high )  2DUP > IF SWAP THEN ;
+
+
+VARIABLE CONNECTIONS
+: LINK ( a -- )  HERE  OVER @ ,  SWAP ! ;
+: CONNECT ( b1 b2 -- )  CONNECTIONS LINK  L>H , ,  0 , ( circuit ) ;
+: 'CIRCUIT ( conn -- n )   3 CELLS + ;
+: MAKE ( n )  CONNECTIONS OFF  \ Make n shortest connections
+    0 ( floor)  SWAP 0 DO  SHORTEST  2DUP CONNECT  DISTANCE  LOOP  DROP ;
+
+: C CONNECT ;
+: .CONN ( conn )  DUP 'CIRCUIT @ .()
+    CELL+ 2@  2DUP SWAP 3 .R ."  - " 3 .R  DISTANCE .() ;
+: CC   CONNECTIONS  BEGIN @ ?DUP WHILE CR DUP .CONN REPEAT ;
+
 VARIABLE CIRCUITS
-: LINK, ( a -- )  HERE  OVER @ ,  SWAP ! ;
+: CIRCUIT ( conn )
+    1 CIRCUITS +!  CIRCUITS @ SWAP 'CIRCUIT ! 
+    
+    BEGIN @ ?DUP WHILE
+        ( does this one belong? )
+    REPEAT ;
 
-: ADD ( b1 b2 circuit -- )  CELL+ LINK, , , ;
+10 MAKE
+CC
 
-: .() ( n )  ."  (" 0 .R ." ) " ;
+quit
 
-: .CONNECTION ( a )  CELL+ 2@  2DUP .BOX ." - " .BOX  DISTANCE .() ;
-: .CIRCUIT ( a )  BEGIN  CR DUP .CONNECTION  @ ?DUP 0= UNTIL ;
-: .CIRCUITS  CIRCUITS BEGIN  @ ?DUP WHILE  DUP CELL+ @ .CIRCUIT  REPEAT ;
+\ Connection:   link, b1, b2
+\ Circuit:      link, connections
+VARIABLE CIRCUITS
+
+: CIRC@ ( circuit -- conn )      CELL+  @ ;
+: CONN@ ( connection -- b1 b2 )  CELL+ 2@ ;
+
+: .CIRCUIT ( circ )  CR ." Circuit:"  CIRC@
+    BEGIN  CR 3 SPACES  DUP .CONNECTION  @ ?DUP 0= UNTIL ;
+: .CIRCUITS  CIRCUITS BEGIN  @ ?DUP WHILE  DUP .CIRCUIT  REPEAT ;
+: C .CIRCUITS ;
+
+\ To add a new connection, if either of the boxes is part of an existing
+\ circuit, add it to that. Otherwise create a new circuit.
+
+: ADD ( b1 b2 circuit -- )  CELL+ LINK,  , , ;
+: NEW ( b1 b2 -- )  HERE  CIRCUITS LINK, 0 ,  ADD ;
+
+: IN-CONNECTION ( b connection -- 0/1 )
+    2DUP CELL+ @ =  -ROT 2 CELLS + @ = OR  NEGATE ;
 
 
-\  : IN-CIRCUIT? ( b circuit -- f )
+: CONNECTS? ( b1 b2 connection -- f )  CONN@ 2>R ( b1 b2 )
+    OVER R@ =  OVER R> = OR  SWAP R@ = OR  SWAP R> = OR ;
+
+T{ CIRCUITS OFF  1 2 NEW -> }T
+T{ 1 6 CIRCUITS @ CIRC@ CONNECTS? -> TRUE }T
+T{ 2 6 CIRCUITS @ CIRC@ CONNECTS? -> TRUE }T
+T{ 3 6 CIRCUITS @ CIRC@ CONNECTS? -> FALSE }T
+
+: BOX-IN-CIRCUIT ( b circuit -- 0/1 )  CIRC@
+    BEGIN  >R  2DUP R@ CONNECTS? IF  R> DROP  2DROP TRUE EXIT  THEN  R>
+    @ ?DUP 0= UNTIL  2DROP FALSE ;
 
 
-\  : CIRCUIT, ( b1 b2 -- )  CIRCUITS LINK, , , ;
+: IN-CIRCUIT? ( b1 b2 circuit -- f )  CIRC@
+    BEGIN  >R  2DUP R@ CONNECTS? IF  R> DROP  2DROP TRUE EXIT  THEN  R>
+    @ ?DUP 0= UNTIL  2DROP FALSE ;
+
+T{ CIRCUITS OFF  1 2 NEW -> }T
+T{ 1 6 CIRCUITS @ IN-CIRCUIT? -> TRUE }T
+T{ 2 6 CIRCUITS @ IN-CIRCUIT? -> TRUE }T
+T{ 3 6 CIRCUITS @ IN-CIRCUIT? -> FALSE }T
+
+\ If one box is in an existing circuit, add to that one
+\ If both boxes are in a circuit, do nothing
+\ If neither box is in a circuit, add a new circuit
+\ : CIRCUIT+ ( b1 b2 circuit -- )
 
 : CONNECT ( b1 b2 -- )
-    CIRCUITS @ 0= IF  CIRCUITS LINK,  0 ,  THEN
-    CIRCUITS @ ADD ;
+    CIRCUITS BEGIN  @ ?DUP WHILE ( b1 b2 circuit )
 
+
+        >R  2DUP R@ IN-CIRCUIT? IF  R> ADD EXIT  THEN  R>
+    REPEAT  NEW ;
